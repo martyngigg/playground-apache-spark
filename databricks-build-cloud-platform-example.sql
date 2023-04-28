edbc9c033d0f9ba2be6641f598a24a0abc488bcb
Martyn Gigg <martyn.gigg@gmail.com>
2023-04-28 09:07:14 +0100

    Continue Databricks example notebook

--- databricks-build-cloud-platform-example.sql
+++ databricks-build-cloud-platform-example.sql
@@ -4,10 +4,11 @@ use dbacademy;
 
 -- COMMAND ----------
 
+-- Create the bronze table that directly looks at the JSON source
 drop table IF EXISTS health_tracker_data_2020_01; 
-create TABLE if not exists health_tracker_data_2020_01
+create TABLE health_tracker_data_2020_01
 using JSON
-location 'dbfs:/FileStore/tables/health_tracker_data_2020_01_dirty.json'
+location 'dbfs:/FileStore/tables/health_tracker_data_2020_01.json'
 options (inferSchema=true);
 
 -- COMMAND ----------
@@ -24,12 +25,20 @@ SELECT * FROM health_tracker_data_2020_01;
 
 -- COMMAND ----------
 
--- JSON is slow to query. Create Silver tables in parquet format,
+-- MAGIC %md **THE NEXT CELL DELETES THE DELTA TABLES ON DISK!!! This for demo purposes. DO NOT DO THIS IN PRODUCTION**
+
+-- COMMAND ----------
+
+-- MAGIC %fs rm -r /dbacademy/DLRS/healthtracker/silver
+
+-- COMMAND ----------
+
+-- JSON is slow to query. Create Silver tables in delta (parquet + transaction) format,
 -- cleaning the data as we go.
 drop table IF EXISTS health_tracker_silver;
 
 create table health_tracker_silver
-USING PARQUET
+USING DELTA
 PARTITIONED BY (p_device_id)
 LOCATION "/dbacademy/DLRS/healthtracker/silver"
 AS (
@@ -38,40 +47,8 @@ AS (
          CAST(FROM_UNIXTIME(timestamp) AS TIMESTAMP) as time,
          CAST(FROM_UNIXTIME(timestamp) AS DATE) as dte,
          device_id AS p_device_id
-  FROM health_tracker_data_2020_01
+  FROM health_tracker_data_2020_01               -- FROM json.'dbfs:/FileStore/tables/health_tracker_data_2020_dirty.json' would avoid creating the bronze table but they can be useful
 )
-
--- COMMAND ----------
-
--- Convert to delta table e.g, makes a copy
-DESCRIBE DETAIL health_tracker_silver;  -- this is in parquet
-
-drop table IF EXISTS health_tracker_silver_delta;
-
-create table health_tracker_silver_delta
-USING DELTA
-PARTITIONED BY (p_device_id)
-LOCATION "/dbacademy/DLRS/healthtracker/silver_delta"
-AS (
-  SELECT *
-  FROM health_tracker_silver
-)
-
--- COMMAND ----------
-
--- Convert original table to delta inplace
-CONVERT TO DELTA
-  parquet.`/dbacademy/DLRS/healthtracker/silver`
-  PARTITIONED BY (p_device_id double)
-
--- COMMAND ----------
-
--- Recreate the table based on delta
-drop table IF EXISTS health_tracker_silver;
-
-CREATE TABLE health_tracker_silver
-USING DELTA
-LOCATION "/dbacademy/DLRS/healthtracker/silver"
 
 -- COMMAND ----------
 
@@ -83,4 +60,141 @@ LOCATION "/dbacademy/DLRS/healthtracker/silver"
 
 -- COMMAND ----------
 
- 
+-- MAGIC %fs ls /dbacademy/DLRS/healthtracker/silver/p_device_id=0/
+
+-- COMMAND ----------
+
+-- MAGIC %fs mounts
+
+-- COMMAND ----------
+
+select * FROM health_tracker_silver
+
+-- COMMAND ----------
+
+DESCRIBE DETAIL health_tracker_silver
+
+-- COMMAND ----------
+
+SELECT COUNT(*) FROM health_tracker_silver
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC **NEXT CELL DELETES OLD FILES SO THE NOTEBOOK WILL RUN IF THE TRAINING HAS BEEN RUN BEFORE. DO NOT DO THIS IN PROD**
+
+-- COMMAND ----------
+
+-- MAGIC %fs rm -r /dbacademy/DLRS/healthtracker/gold
+
+-- COMMAND ----------
+
+DROP TABLE IF EXISTS health_tracker_user_analytics;
+
+-- Create data mart for basic summary data. These tables are required for performance to avoid
+-- recomputing expensive analytics on large datasets but may not be necessary...
+CREATE TABLE health_tracker_user_analytics
+USING DELTA
+LOCATION 'dbfs:/dbacademy/DLRS/healthtracker/gold/health_tracker_user_analytics'
+AS (
+  SELECT p_device_id,
+         AVG(heartrate) AS avg_heartrate,
+         STD(heartrate) AS std_heartrate,
+         MAX(heartrate) AS max_heartrate
+  FROM health_tracker_silver GROUP BY p_device_id
+)
+
+-- COMMAND ----------
+
+select * from health_tracker_user_analytics order by p_device_id
+
+-- COMMAND ----------
+
+-- Create the bronze table that directly looks at the second JSON source
+drop table IF EXISTS health_tracker_data_2020_02; 
+create TABLE health_tracker_data_2020_02
+using JSON
+location 'dbfs:/FileStore/tables/health_tracker_data_2020_02.json'
+options (inferSchema=true);
+
+-- Insert new data to existing silver tables
+INSERT INTO health_tracker_silver
+ SELECT name,
+        heartrate,
+        CAST(FROM_UNIXTIME(timestamp) AS TIMESTAMP) as time,
+        CAST(FROM_UNIXTIME(timestamp) AS DATE) as dte,
+        device_id AS p_device_id
+FROM health_tracker_data_2020_02
+
+-- COMMAND ----------
+
+show tables;
+
+-- COMMAND ----------
+
+DESCRIBE HISTORY health_tracker_silver;
+
+-- COMMAND ----------
+
+-- MAGIC %fs ls dbfs:/dbacademy/DLRS/healthtracker/silver/_delta_log
+
+-- COMMAND ----------
+
+SELECT COUNT(*) FROM health_tracker_silver
+
+-- COMMAND ----------
+
+SELECT COUNT(*) FROM health_tracker_silver VERSION AS OF 0
+
+-- COMMAND ----------
+
+-- Count number of records by device
+SELECT p_device_id, COUNT(*) FROM health_tracker_silver GROUP BY p_device_id ORDER BY p_device_id
+
+-- COMMAND ----------
+
+-- Identify missing records
+SELECT * FROM health_tracker_silver WHERE p_device_id IN (3,4)
+
+-- COMMAND ----------
+
+-- Identify broken records (heartrate < 0)
+-- Use view rather than table on disk
+CREATE OR REPLACE TEMPORARY VIEW broken_readings
+AS (
+  SELECT p_device_id, heartrate, time FROM health_tracker_silver
+  WHERE heartrate < 0
+  ORDER BY time
+)
+
+-- COMMAND ----------
+
+SELECT * FROM broken_readings ORDER BY p_device_id
+
+-- COMMAND ----------
+
+SELECT SUM(broken_readings_count) FROM broken_readings
+
+-- COMMAND ----------
+
+-- UPSERT to fix broken data
+
+CREATE OR REPLACE TEMPORARY VIEW updates
+AS (
+  SELECT name, (prev_amt+next_amt)/2 AS heartrate, time, dte, p_device_id
+  FROM (
+    SELECT *,
+    LAG(heartrate) OVER (PARTITION BY p_device_id, time ORDER BY p_device_id, time) as prev_amt,
+    LEAD(heartrate) OVER (PARTITION BY p_device_id, time ORDER BY p_device_id, time) as next_amt
+    FROM health_tracker_silver
+  )
+  WHERE heartrate < 0
+)
+
+-- COMMAND ----------
+
+select * from updates;
+
+-- COMMAND ----------
+
+
